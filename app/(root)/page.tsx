0b7d6c7d569cabe4890432cbaa54ea83daf82385
seyaendris <seide9894@gmail.com>
2025-03-22 15:17:55 +0300

    Project setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center mt-32">
+      <h1 className="font-bold text-3xl">Welcome to the final year  project</h1>
+    </div>
+  );
+}
