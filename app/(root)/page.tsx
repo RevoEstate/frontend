1bae3504c9af627ab514172368b464b607085837
seyaendris <seide9894@gmail.com>
2025-03-22 15:36:23 +0300

    Framer Motion for animations

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+"use client";
+
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center mt-32">
-      <h1 className="font-bold text-3xl">Welcome to the final year  project</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 to-purple-100">
+      <motion.h1
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        // whileHover={{ scale: 1.1 }}
+        className="text-white font-bold text-4xl md:text-5xl text-center drop-shadow-lg"
+      >
+        Welcome to the Final Year Project 🚀
+      </motion.h1>
     </div>
   );
 }
