33cc9330dce169c16e94f52a68294e7e7e2f19cf
seyaendris <seide9894@gmail.com>
2025-03-30 16:42:30 +0300

    integrated the hero and the navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,14 @@
-"use client";
+import FeaturedProperties from '@/components/FeaturedProperties'
+import Hero from '@/components/Hero'
+import React from 'react'
 
-import { motion } from "framer-motion";
-
-export default function Home() {
+const page = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 to-purple-100">
-      <motion.h1
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, ease: "easeOut" }}
-        // whileHover={{ scale: 1.1 }}
-        className="text-white font-bold text-4xl md:text-5xl text-center drop-shadow-lg"
-      >
-        Welcome to the Final Year Project 🚀
-      </motion.h1>
-    </div>
-  );
+    <>
+      <Hero />
+      <FeaturedProperties />
+    </>
+  )
 }
+
+export default page
