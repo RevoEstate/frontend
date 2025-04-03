bcbeee29d0163eb24771a7c4aa83ca2c1e43669b
seyaendris <seide9894@gmail.com>
2025-04-03 19:49:32 +0300

    craeted root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,22 +1,4 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
 import Navbar from "@/components/Navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "RevoEstate",
-  description: "Find the best properties with 360° virtual tours and AI-powered recommendations.",
-};
 
 export default function RootLayout({
   children,
@@ -24,13 +6,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        {children}
-      </body>
-    </html>
+    <div className='flex h-screen flex-col'>
+      <Navbar />
+      <main className='flex-1 wrapper'>{children}</main>
+    </div>
   );
 }
