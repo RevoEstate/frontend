e6f89f295534fa6f639c92759a304b1a9e310c31
seyaendris <seide9894@gmail.com>
2025-04-06 20:20:29 +0300

    server url updated

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,5 @@
-import { customerSignupSchema } from "@/lib/validators";
+import { customerSignupSchema, realEstateSignupSchema } from "@/lib/validators";
 import { z } from "zod";
 
 export type CustomerSignupFormData = z.infer<typeof customerSignupSchema>;
+export type RealEstateSignupFormData = z.infer<typeof realEstateSignupSchema>;
