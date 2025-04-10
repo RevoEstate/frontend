40cf711a49795c25d1bfac095f41e8d9af2e3ff5
seyaendris <seide9894@gmail.com>
2025-04-10 17:14:34 +0300

    build error solved

---
+++ app/(auth)/signup-realestate/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RealestateSignupPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RealestateSignupPage
---
+++ app/(auth)/signup-realestate/realestate-signup-form.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RealestateSignupForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RealestateSignupForm
