8ba70046322ff5b9cf44c807a54f72c0c9da005c
Jibril Arbicho <jibrilarbicho2@gmail.com>
2025-04-17 17:49:56 +0300

    usessesion

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
 import FeaturedProperties from "@/components/FeaturedProperties";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import { useSession } from "@/lib/auth-client";
 import React from "react";
 
 const page = () => {
+  const { data: session, status } = useSession();
+  console.log("Session data:", session);
+  console.log("Session user:", session?.user);
+
   return (
     <>
       <Navbar />
