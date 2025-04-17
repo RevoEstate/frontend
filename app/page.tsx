b5b75fc939063e679e94032eb359073bd9440701
Jibril Arbicho <jibrilarbicho2@gmail.com>
2025-04-17 17:15:12 +0300

    Signup,verification after Signup,sigin-iin Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import FeaturedProperties from "@/components/FeaturedProperties";
 import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 const page = () => {
   return (
     <>
+      <Navbar />
       <Hero />
       <FeaturedProperties />
     </>
