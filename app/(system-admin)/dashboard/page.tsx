90fd6bfd3e550dd4b8ba4752bda3f72965ae506a
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2025-04-23 02:13:27 +0300

    Enhance admin dashboard metadata and improve header UI with a welcome message

--- app/(system-admin)/dashboard/page.tsx
+++ app/(system-admin)/dashboard/page.tsx
@@ -1,6 +1,11 @@
-import type { Metadata } from "next";
-import { EcommerceMetrics } from "@/components/ecommerce/EcommerceMetrics";
 import React from "react";
+import type { Metadata } from "next";
+import {
+  APP_DESCRIPTION,
+  APP_NAME,
+  SERVER_URL,
+} from "@/lib/constants/index.ts";
+import { EcommerceMetrics } from "@/components/ecommerce/EcommerceMetrics";
 import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
 import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
 import StatisticsChart from "@/components/ecommerce/StatisticsChart";
@@ -8,9 +13,27 @@ import RecentOrders from "@/components/ecommerce/RecentOrders";
 import DemographicCard from "@/components/ecommerce/DemographicCard";
 
 export const metadata: Metadata = {
-  title:
-    "Next.js E-commerce Dashboard | Revostate - Next.js Dashboard Template",
-  description: "This is Next.js Home for Revostate Dashboard Template",
+  title: {
+    template: `%s | RevoEstate`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
+  openGraph: {
+    title: APP_NAME,
+    description: APP_DESCRIPTION,
+    url: SERVER_URL,
+    siteName: APP_NAME,
+    images: [
+      {
+        url: `${SERVER_URL}/og-image.png`,
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
 };
 
 export default function Ecommerce() {
